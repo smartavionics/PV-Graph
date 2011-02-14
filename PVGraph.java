ad05ca447a6f4f2be2adb9730c859d38504d6f50
Mark Burton <markb@smartavionics.com>
2011-02-14 11:52:53 +0000

    Now plots data for multiple inverters - each has own line.
    Also, database connection is now closed from the windowClosing() callback
    rather than from within main().

--- PVGraph.java
+++ PVGraph.java
@@ -40,101 +40,103 @@ public class PVGraph extends ApplicationFrame {
     private Calendar date; 
     
     public PVGraph(Connection conn) {
-    	super("PV Power");
-    	this.conn = conn;
-    	synchronized(graphs) {
-    	    graphs.add(this);
-    	}
-    	
+        super("PV Power");
+        this.conn = conn;
+        synchronized(graphs) {
+            graphs.add(this);
+        }
+
         date = new GregorianCalendar();
-    	final ChartPanel chartPanel = (ChartPanel)createDayChartPanel();
-    	chartPanel.setPreferredSize(new java.awt.Dimension(800, 500));
-    	JPanel mainPanel = new JPanel();
-    	mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-    	mainPanel.add(chartPanel);
-    	JPanel buttonsPanel = new JPanel();
-    	JButton dayDecButton = new JButton("Day -");
-    	dayDecButton.addActionListener(new ActionListener() {
-    		public void actionPerformed(ActionEvent event) {
-    		    date.add(Calendar.DAY_OF_MONTH, -1);
-    		    chartPanel.setChart(createDayChart());;
-    		}
-    	});
-    	JButton dayIncButton = new JButton("Day +");
-    	dayIncButton.addActionListener(new ActionListener() {
-    		public void actionPerformed(ActionEvent event) {
-    		    date.add(Calendar.DAY_OF_MONTH, 1);
-    		    chartPanel.setChart(createDayChart());;
-    		}
-    	});
-    	
-    	JButton monthDecButton = new JButton("Month -");
-    	monthDecButton.addActionListener(new ActionListener() {
-    		public void actionPerformed(ActionEvent event) {
-    		    date.add(Calendar.MONTH, -1);
-    		    chartPanel.setChart(createDayChart());;
-    		}
-    	});
-    	JButton monthIncButton = new JButton("Month +");
-    	monthIncButton.addActionListener(new ActionListener() {
-    		public void actionPerformed(ActionEvent event) {
-    		    date.add(Calendar.MONTH, 1);
-    		    chartPanel.setChart(createDayChart());;
-    		}
-    	});
-    	    	
-    	JButton yearDecButton = new JButton("Year -");
-    	yearDecButton.addActionListener(new ActionListener() {
-    		public void actionPerformed(ActionEvent event) {
-    		    date.add(Calendar.YEAR, -1);
-    		    chartPanel.setChart(createDayChart());;
-    		}
-    	});
-    	JButton yearIncButton = new JButton("Year +");
-    	yearIncButton.addActionListener(new ActionListener() {
-    		public void actionPerformed(ActionEvent event) {
-    		    date.add(Calendar.YEAR, 1);
-    		    chartPanel.setChart(createDayChart());;
-    		}
-    	});
-    	
-    	buttonsPanel.add(yearDecButton);
-    	buttonsPanel.add(yearIncButton);
-    	buttonsPanel.add(monthDecButton);
-    	buttonsPanel.add(monthIncButton);
-    	buttonsPanel.add(dayDecButton);
-    	buttonsPanel.add(dayIncButton);
-    	
-    	mainPanel.add(buttonsPanel);
-    	setContentPane(mainPanel);
+        final ChartPanel chartPanel = (ChartPanel)createDayChartPanel();
+        chartPanel.setPreferredSize(new java.awt.Dimension(800, 500));
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        mainPanel.add(chartPanel);
+        JPanel buttonsPanel = new JPanel();
+        JButton dayDecButton = new JButton("Day -");
+        dayDecButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent event) {
+                    date.add(Calendar.DAY_OF_MONTH, -1);
+                    chartPanel.setChart(createDayChart());;
+                }
+        });
+        JButton dayIncButton = new JButton("Day +");
+        dayIncButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent event) {
+                    date.add(Calendar.DAY_OF_MONTH, 1);
+                    chartPanel.setChart(createDayChart());;
+                }
+        });
+        
+        JButton monthDecButton = new JButton("Month -");
+        monthDecButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent event) {
+                    date.add(Calendar.MONTH, -1);
+                    chartPanel.setChart(createDayChart());;
+                }
+        });
+        JButton monthIncButton = new JButton("Month +");
+        monthIncButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent event) {
+                    date.add(Calendar.MONTH, 1);
+                    chartPanel.setChart(createDayChart());;
+                }
+        });
+        
+        JButton yearDecButton = new JButton("Year -");
+        yearDecButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent event) {
+                    date.add(Calendar.YEAR, -1);
+                    chartPanel.setChart(createDayChart());;
+                }
+        });
+        JButton yearIncButton = new JButton("Year +");
+        yearIncButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent event) {
+                    date.add(Calendar.YEAR, 1);
+                    chartPanel.setChart(createDayChart());;
+                }
+        });
+        
+        buttonsPanel.add(yearDecButton);
+        buttonsPanel.add(yearIncButton);
+        buttonsPanel.add(monthDecButton);
+        buttonsPanel.add(monthIncButton);
+        buttonsPanel.add(dayDecButton);
+        buttonsPanel.add(dayIncButton);
+        
+        mainPanel.add(buttonsPanel);
+        setContentPane(mainPanel);
+        pack();
+        setVisible(true);
     }
     
     public JPanel createDayChartPanel() {
-    	JFreeChart chart = createDayChart();
-    	ChartPanel panel = new ChartPanel(chart);
-    	panel.setFillZoomRectangle(true);
-    	panel.setMouseWheelEnabled(true);
-    	return panel;
+        JFreeChart chart = createDayChart();
+        ChartPanel panel = new ChartPanel(chart);
+        panel.setFillZoomRectangle(true);
+        panel.setMouseWheelEnabled(true);
+        return panel;
     }
     
     private JFreeChart createDayChart() {
-    	
-    	int year = date.get(Calendar.YEAR);
-    	int month = date.get(Calendar.MONTH) + 1;
-    	int day = date.get(Calendar.DAY_OF_MONTH);
-    	
-    	XYDataset dataset = createDayDataset(year, month, day);
-    	
-    	JFreeChart chart = ChartFactory.createTimeSeriesChart(
-    	    year + "/" + month + "/" + day, // title
-    	    "Time",     // x-axis label
-    	    "Watts",    // y-axis label
-    	    dataset,    // data
-    	    true,       // create legend?
-    	    true,       // generate tooltips?
-    	    false       // generate URLs?
-    	    );
-    	
+        
+        int year = date.get(Calendar.YEAR);
+        int month = date.get(Calendar.MONTH) + 1;
+        int day = date.get(Calendar.DAY_OF_MONTH);
+        
+        XYDataset dataset = createDayDataset(year, month, day);
+        
+        JFreeChart chart = ChartFactory.createTimeSeriesChart(
+            year + "/" + month + "/" + day, // title
+            "Time",     // x-axis label
+            "Watts",    // y-axis label
+            dataset,    // data
+            true,       // create legend?
+            true,       // generate tooltips?
+            false       // generate URLs?
+            );
+        
         chart.setBackgroundPaint(Color.white);
         
         XYPlot plot = (XYPlot) chart.getPlot();
@@ -158,9 +160,9 @@ public class PVGraph extends ApplicationFrame {
             renderer.setBaseShapesVisible(true);
             renderer.setBaseShapesFilled(true);
             renderer.setDrawSeriesLineAsPath(true);
-            renderer.setSeriesPaint(0, new Color(0, 128, 0));
-            //renderer.setSeriesShapesVisible(0, false);
-            renderer.setSeriesShape(0, new Rectangle(-1, -1, 2, 2));
+            //renderer.setSeriesPaint(0, new Color(0, 128, 0));
+            for(int i = 0; i < dataset.getSeriesCount(); ++i)
+                renderer.setSeriesShape(i, new Rectangle(-2, -2, 4, 4));
         }
         
         DateAxis axis = (DateAxis) plot.getDomainAxis();
@@ -168,97 +170,97 @@ public class PVGraph extends ApplicationFrame {
         
         return chart;   
     }
-
+    
+    class DayData {
+        String inverter;
+        String serial;
+        java.util.List<Timestamp> times = new java.util.ArrayList<Timestamp>(12 * 24);
+        java.util.List<Integer> powers = new java.util.ArrayList<Integer>(12 * 24);
+    };
+    
     private XYDataset createDayDataset(int year, int month, int day) {
-    	
-    	java.util.List<Timestamp> times = new ArrayList<Timestamp>(24 * 12);
-    	java.util.List<Integer> powers = new ArrayList<Integer>(24 * 12);
-    	getDayData(year, month, day, times, powers);
-    	//System.err.println("Got " + dayData.size() + " values");
-
-        TimeSeries s1 = new TimeSeries(year + "/" + month + "/" + day);
-        for(int i = 0; i < times.size(); ++i)
-            s1.add(new Minute(times.get(i)), powers.get(i));
+        
         TimeSeriesCollection dataset = new TimeSeriesCollection();
-        dataset.addSeries(s1);
-
+        
+        java.util.List<DayData> dayData = getDayData(year, month, day);
+        for(DayData dd : dayData) {
+            TimeSeries s = new TimeSeries(dd.inverter + (dayData.size() > 1? ("-" + dd.serial) : ""));
+            for(int i = 0; i < dd.times.size(); ++i)
+                s.add(new Minute(dd.times.get(i)), dd.powers.get(i));
+            dataset.addSeries(s);
+        }
+        
         return dataset;
     }
-
+    
     public void windowClosing(java.awt.event.WindowEvent event) {
-    	synchronized(graphs) {
-    	    graphs.remove(this);
-    	}
-    	super.windowClosing(event);
+        synchronized(graphs) {
+            graphs.remove(this);
+            if(graphs.size() == 0) {
+                try {
+                    conn.close();
+                    System.out.println("Database connection terminated");
+                }
+                catch(Exception e) {
+                    // relax
+                }
+            }
+        }
+        super.windowClosing(event);
     }
     
-    public void getDayData(int year, int month, int day, java.util.List<Timestamp> times, java.util.List<Integer> powers) {
-    	Statement stmt = null;
-    	String query = "select * from DayData where year(DateTime) = " + year + " and month(DateTime) = " + month + " and dayofmonth(DateTime) = " + day + " order by DateTime";
-    	try {
-    	    stmt = conn.createStatement();
-    	    ResultSet rs = stmt.executeQuery(query);
-    	    while (rs.next()) {
-    	    	times.add(rs.getTimestamp("DateTime"));
-    	    	powers.add(rs.getInt("CurrentPower"));
+    public java.util.List<DayData> getDayData(int year, int month, int day) {
+        Statement stmt = null;
+        String query = "select * from DayData where year(DateTime) = " + year + " and month(DateTime) = " + month + " and dayofmonth(DateTime) = " + day + " order by DateTime";
+        Map<String, DayData> result = new HashMap<String, DayData>();
+        try {
+            stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                String serial = rs.getString("serial");
+                DayData dd = result.get(serial);
+                if(dd == null) {
+                    dd = new DayData();
+                    dd.serial = serial;
+                    dd.inverter = rs.getString("inverter");
+                    result.put(serial, dd);
+                }
+                dd.times.add(rs.getTimestamp("DateTime"));
+                dd.powers.add(rs.getInt("CurrentPower"));
             }
-    	} catch (SQLException e ) {
-    	    System.err.println("Query failed: " + e.getMessage());
-    	} finally {
-    	    try {
-    	    	stmt.close();
-    	    }
-    	    catch (SQLException e) {
-    	    	// relax
-    	    }
-    	}
+        } catch (SQLException e ) {
+            System.err.println("Query failed: " + e.getMessage());
+        } finally {
+            try {
+                stmt.close();
+            }
+            catch (SQLException e) {
+                // relax
+            }
+        }
+        return new java.util.ArrayList<DayData>(result.values());
     }
     
     public static void main (String[] args) {
-    	
-    	props = new Properties(System.getProperties());
-    	try {
-    	    props.load(new FileInputStream("pvgraph.properties"));
-    	}
-    	catch (IOException ioe) {
-    	    // relax
-    	}
-    	
-    	Connection conn = null;
-    	try {
-    	    String user = props.getProperty("user");
-    	    String password = props.getProperty("password");
-    	    String url = props.getProperty("url");
-    	    Class.forName ("com.mysql.jdbc.Driver").newInstance();
-    	    conn = DriverManager.getConnection (url, user, password);
-    	    System.out.println ("Database connection established");
-    	    PVGraph pvgraph = new PVGraph(conn);
-    	    pvgraph.pack();
-    	    pvgraph.setVisible(true);
-    	    boolean finished = false;
-    	    while(!finished) {
-    	    	try {
-    	    	    Thread.sleep(100);	
-    	    	}
-    	    	catch (InterruptedException ie) {
-    	    	    // relax
-    	    	}
-    	    	synchronized(graphs) {
-    	    	    finished = (graphs.size() == 0);
-    	    	}
-    	    }
-    	}
-    	catch (Exception e) {
-    	    System.err.println("Cannot connect to database server: " + e.getMessage());
-    	}
-    	finally {
-    	    if (conn != null) {
-    	    	try {
-    	    	    conn.close();
-    	    	    System.out.println("Database connection terminated");
-    	    	}
-    	    	catch (Exception e) { /* ignore close errors */ }
-    	    }
-    	}
+        props = new Properties(System.getProperties());
+        try {
+            props.load(new FileInputStream("pvgraph.properties"));
+        }
+        catch (IOException ioe) {
+            // relax
+        }
+        String user = props.getProperty("user");
+        String password = props.getProperty("password");
+        String url = props.getProperty("url");
+        Connection conn = null;
+        try {
+            Class.forName ("com.mysql.jdbc.Driver").newInstance();
+            conn = DriverManager.getConnection (url, user, password);
+            System.out.println ("Database connection established");
+            new PVGraph(conn);
+        }
+        catch (Exception e) {
+            System.err.println("Cannot connect to " + url + ": " + e.getMessage());
+        }
     }
 }
